48984fc965e49bb8fd884cbda5161ed8a1a3b903
VVNTHEFLASH <vishnurockhunters@gmail.com>
2022-06-30 22:38:21 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import Pricing from './components/Pricing';
+import Feedback from './components/Feedback';
+import Aboutus from './components/Aboutus';
 
 function App() {
   return (
+    <>
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <Routes>
+        <Route exact path='/' element={<Home/>}></Route>
+        <Route path='/home' element={<Home/>}></Route>
+        <Route path='/pricing' element={<Pricing/>}></Route>
+        <Route path='/feedback' element={<Feedback/>}></Route>
+        <Route path='/aboutus' element={<Aboutus/>}></Route>
+      </Routes>
     </div>
+    </BrowserRouter>
+    </>
   );
 }
 
